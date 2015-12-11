2063d06f7970bc9a466f61382422709b31375dc4
Stephan Bijzitter <Stephan@skelware.com>
2015-12-11 12:22:38 +0100

    Adhere to naming conventions for CSV

--- spec/parsers/csv.spec.js
+++ spec/parsers/csv.spec.js
@@ -5,7 +5,7 @@ describe('CSV Parser', function() {
     var parser_a = NodeFileParser.link('data/valid/lorem.csv');
     var parser_b = NodeFileParser.link('data/valid/lorem.csv');
     var parser_c = NodeFileParser.link('data/valid/lorem.csv', {
-        separator: ','
+        delimiter: ','
     });
     var parser_d = NodeFileParser.link('data/invalid/lorem.csv');
 
@@ -13,8 +13,8 @@ describe('CSV Parser', function() {
         expect(parser_a).not.toBe(parser_b);
     });
 
-    it('should use a comma as default separator', function() {
-        expect(parser_a._separator).toEqual(parser_c._separator);
+    it('should use a comma as default delimiter', function() {
+        expect(parser_a._delimiter).toEqual(parser_c._delimiter);
     });
 
     it('should be able to read a CSV file', function() {
@@ -24,7 +24,7 @@ describe('CSV Parser', function() {
         expect(content_a && content_a.splice).toBeDefined();
     });
 
-    it('should discard invalid entries silently', function() {
+    it('should discard invalid records silently', function() {
         expect(function() {
             parser_d.read().getContent();
         }).not.toThrow();
