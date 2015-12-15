35685bfabb862e21c77aab7e8571096a4fa2386d
Stephan Bijzitter <Stephan@skelware.com>
2015-12-15 16:16:38 +0100

    Add Commander to parse command-line options

--- cli/NodeFileParserCLI.js
+++ cli/NodeFileParserCLI.js
@@ -2,4 +2,8 @@
 
 (function NodeFileParserCLI() {
 
+    var program = require('commander');
+    var pkg = require('./../package.json');
+
+    program.version(pkg.version).parse(process.argv);
 }());
