9ba05610346e1a889db17f8d350c3d5c33180c07
Stephan Bijzitter <Stephan@skelware.com>
2015-12-23 19:30:27 +0100

    Add a nice debug message to show it actually works

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,11 @@
 
 set -e;
 
-if ! { [ "$TRAVIS_REPO_SLUG" == "$MY_REPO_SLUG" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_TAG" ]; };
+if [ "$TRAVIS_REPO_SLUG" == "$MY_REPO_SLUG" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_TAG" ];
     then
+        echo "Deploying v${TRAVIS_TAG}!";
+    else
+        echo "This build does not require deployment.";
         exit 0;
 fi;
 
