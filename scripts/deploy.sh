2abfd9f30191f4dd398d27cb904ea8bf15ba5d06
Stephan Bijzitter <Stephan@skelware.com>
2015-12-23 20:08:17 +0100

    Move conditions into the build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,6 @@
 
 set -e;
 
-if [ "$TRAVIS_REPO_SLUG" == "$MY_REPO_SLUG" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_TAG" ];
-    then
-        echo "Deploying v${TRAVIS_TAG}!";
-    else
-        echo "This build does not require deployment.";
-        exit 0;
-fi;
-
 bash ./scripts/build.sh;
 
 cd build/documentation;
