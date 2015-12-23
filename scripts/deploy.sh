eab60003add95d1fdb7944dc8ae4dcea6685df26
Stephan Bijzitter <Stephan@skelware.com>
2015-12-23 19:02:46 +0100

    Add bash deploy script to generate documentation

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e;
+
+if ! { [ "$TRAVIS_REPO_SLUG" == "$MY_REPO_SLUG" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_TAG" ]; };
+    then
+        exit 0;
+fi;
+
+sh ./scripts/compile.sh;
+
+cd build/documentation;
+git init;
+
+git config user.name "SkelwareBOT";
+git config user.email "bot@skelware.com";
+
+git add .;
+git commit -m "Deploy ${TRAVIS_TAG}";
+
+git push --force --quiet "https://${GITHUB_TOKEN}@${MY_REPO_HOST}${MY_REPO_SLUG}" master:gh-pages > /dev/null 2>&1;
